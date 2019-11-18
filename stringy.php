f82d3911667519d8d9b11d4611cf62311661f031
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-18 14:17:12 +0000

    Task Five: Creating a class of Stringy and writing functions to change to lowercase, uppercase, append to end and repeat.

---
+++ stringy.php
@@ -0,0 +1,58 @@
+<?php
+
+// ... your classes here
+class Stringy
+{
+
+    private $string;
+
+    public function __construct($string)
+    {
+
+        $this->string = $string;
+
+    }
+
+    public function lower()
+    {
+
+        return strtolower($this->string);
+
+    }
+
+    public function upper()
+    {
+
+        return strtoupper($this->string);
+
+    }
+
+    public function append($end)
+    {
+
+       return $this->string . $end;
+
+    }
+
+    public function repeat($times)
+    {
+
+        return str_repeat($this->string, $times);
+
+    }
+
+}
+
+$string = new Stringy("Na");
+
+// it can lowercase a string
+var_dump($string->lower()); // string(2) "na"
+
+// it can uppercase a string
+var_dump($string->upper()); // string(2) "NA"
+
+// it can concatenate
+var_dump($string->append("blah")); // string(6) "Nablah"
+
+// // it can repeat a string
+var_dump($string->repeat(5)); // string(10) "NaNaNaNaNa"
